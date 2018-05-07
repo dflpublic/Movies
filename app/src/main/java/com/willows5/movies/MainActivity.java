1b8d515e4e5b6662a3d8a2ebd1ae8500a0035c64
dflpublic <dfl@yahoo.com>
2018-05-07 11:24:59 -0700

    AsyncTask -> AsyncLoader

--- app/src/main/java/com/willows5/movies/MainActivity.java
+++ app/src/main/java/com/willows5/movies/MainActivity.java
@@ -1,13 +1,19 @@
 package com.willows5.movies;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,18 +39,22 @@ import butterknife.ButterKnife;
 //done: create horizontal layout for MainActivity
 //done: create horizontal layout for DetailActivity
 
-public class MainActivity extends AppCompatActivity implements MoviesAdapterOnClickHandler {
+public class MainActivity extends AppCompatActivity implements MoviesAdapterOnClickHandler,
+                                                               LoaderManager
+                                                                       .LoaderCallbacks<Movie[]> {
     @BindView(R.id.rvMain)
     RecyclerView rvMain;
-    @BindView(R.id.tv_error_message_display)
-    TextView tvError;
-    @BindView(R.id.pb_loading_indicator)
-    ProgressBar progressBar;
+    static final int    LOADER_ID = 42;
+    static final String TAG       = MainActivity.class.getSimpleName();
     MoviesAdapter _adapter;
 
     static final String SORT      = "sort";
     static final String POPULAR   = "popular";
     static final String TOP_RATED = "top_rated";
+    @BindView(R.id.tv_error_message_display)
+    TextView    tvError;
+    @BindView(R.id.pb_loading_indicator)
+    ProgressBar progressBar;
 
     String sSort;
 
@@ -60,6 +70,10 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapterOnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        ActionBar ab = getActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
         int               nCols         = getNumColumns(this);
         GridLayoutManager layoutManager = new GridLayoutManager(this, nCols);
@@ -69,6 +83,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapterOnCl
         _adapter = new MoviesAdapter(this);
         rvMain.setAdapter(_adapter);
 
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
         loadMovies();
     }
 
@@ -77,10 +92,19 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapterOnCl
         grab saved preference to determine which way to sort
          */
         showMovies();
-        SharedPreferences pref  = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences pref = getPreferences(Context.MODE_PRIVATE);
         sSort = pref.getString(SORT, POPULAR);
 
-        new FetchMoviesTask().execute(sSort);
+        Bundle bundle = new Bundle();
+        bundle.putString(SORT, sSort);
+
+        LoaderManager   loaderManager = getSupportLoaderManager();
+        Loader<Movie[]> loader        = loaderManager.getLoader(LOADER_ID);
+        if (loader == null) {
+            loaderManager.initLoader(LOADER_ID, bundle, this);
+        } else {
+            loaderManager.restartLoader(LOADER_ID, bundle, this);
+        }
     }
 
     private void setSort(String sWhat) {
@@ -137,7 +161,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapterOnCl
         supported
          */
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        //float          dpWidth    = metrics.widthPixels / metrics.density;
+
         int imageWidth = getResources().getDimensionPixelSize(R.dimen.thumbnail_image_width);
         return metrics.widthPixels / imageWidth;
     }
@@ -162,44 +186,67 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapterOnCl
         rvMain.setVisibility(View.INVISIBLE);
     }
 
-    public class FetchMoviesTask extends AsyncTask<String, Void, Movie[]> {
+    @NonNull
+    @Override
+    public Loader<Movie[]> onCreateLoader(int id, @Nullable final Bundle args) {
+        return new AsyncTaskLoader<Movie[]>(this) {
+            Movie[] movies;
 
-        @Override
-        protected Movie[] doInBackground(String... params) {
-            if (params.length == 0) {
+            @Override
+            protected void onStartLoading() {
+                if (args == null) {
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                if (movies != null) {
+                    deliverResult(movies);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Nullable
+            @Override
+            public Movie[] loadInBackground() {
+                String sWhat = args.getString(SORT);
+                if (sWhat == null || TextUtils.isEmpty(sWhat)) {
+                    return null;
+                }
+                URL requestUrl = Movie.buildUrlMovie(sWhat);
+                try {
+                    String s = NetworkUtils.getResponseFromHttpUrl(requestUrl);
+                    if (s != null) {
+                        return Movie.getMovieData(s);
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
                 return null;
             }
 
-            String sWhat = params[0];
+            @Override
+            public void deliverResult(@Nullable Movie[] data) {
+                movies = data;
+                super.deliverResult(data);
+            }
+        };
+    }
 
-            URL requestUrl = Movie.buildUrlMovie(sWhat);
-            try {
-                String s = NetworkUtils.getResponseFromHttpUrl(requestUrl);
-                if (s != null) {
-                    return Movie.getMovieData(s);
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+    @Override
+    public void onLoadFinished(@NonNull Loader<Movie[]> loader, Movie[] data) {
+        progressBar.setVisibility(View.INVISIBLE);
+        if (data != null) {
+            showMovies();
+            _adapter.setMovies(data);
+        } else {
+            showError();
         }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressBar.setVisibility(View.VISIBLE);
-        }
+    }
 
-        @Override
-        protected void onPostExecute(Movie[] movies) {
-            progressBar.setVisibility(View.INVISIBLE);
-            if (movies != null) {
-                showMovies();
-                _adapter.setMovies(movies);
-            } else {
-                showError();
-            }
-        }
+    @Override
+    public void onLoaderReset(@NonNull Loader<Movie[]> loader) {
+
     }
 }
